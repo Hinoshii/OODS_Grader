da0888ac1171f9cc8c7fa61fc0af27a6d3521b88
Hinoshii_38 <thapatw.bs@gmail.com>
2023-10-09 19:55:36 +0700

    f

--- Search/Search5.py
+++ Search/Search5.py
@@ -3,13 +3,35 @@ def getDistance(cord1,cord2):
     return dis
 #Enter a list of points: 1 1,2 2,3 3/1 1
 inp, start = input("Enter a list of points: ").split("/")
-start = tuple(map(int,start.split()))
+start = tuple(map(float,start.split()))
 inp = inp.split(",")
 cords = []
 for i in inp:
-    i = list(map(int,i.split()))
+    i = list(map(float,i.split()))
     i = tuple(i)
     cords.append(i)
-print(cords,start)
 
-getDistance(cords[1],start)
+if start not in cords:
+    #[0.0, 0.0] is not in [[123.0, 456.0], [-4627.0, 9921.0], [0.0, -716.0], [40.0, 32.0], [1.0, 738.0], [83.0, 0.0], [6321.0, 342.0]]
+    print()
+    exit()
+
+mindis = 5000000
+closecords = 0
+while(len(cords) != 1):
+    for i in cords:
+        dis = getDistance(i,start)
+        if dis < mindis and dis != 0:
+            mindis = dis
+            closecords = i
+
+    #[1.0, 1.0] -> [2.0, 2.0] | The distance is 1.4142
+    print("[" + "{:.1f}".format(start[0]) + ", " + "{:.1f}".format(start[1]) + "] -> [" + "{:.1f}".format(closecords[0]) + ", " + "{:.1f}".format(closecords[1]) + "]",end="")
+    print(" | The distance is " + "{:.4f}".format(mindis))
+
+    cords.remove(start)
+    start = closecords
+    closecords = 0
+    mindis = 5000000
+
+
